b906f47a118e0acda0ad946802c058ce23aad6f7
Thullyoo <thullyofilho@gmail.com>
2024-07-14 20:23:46 -0300

    feat: adicionado get para redirecionamento do link encurtado

--- src/main/java/com/thullyoo/encurtaUrl/controllers/LinkController.java
+++ src/main/java/com/thullyoo/encurtaUrl/controllers/LinkController.java
@@ -1,15 +1,18 @@
 package com.thullyoo.encurtaUrl.controllers;
 
+import com.thullyoo.encurtaUrl.entities.Link;
 import com.thullyoo.encurtaUrl.entities.LinkRequest;
 import com.thullyoo.encurtaUrl.entities.LinkResponse;
 import com.thullyoo.encurtaUrl.services.LinkService;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 public class LinkController {
@@ -20,5 +23,12 @@ public class LinkController {
     @PostMapping()
     public ResponseEntity<LinkResponse> criarUrlEncurtada(@RequestBody LinkRequest linkRequest){
         return ResponseEntity.status(HttpStatus.CREATED).body(this.linkService.criarUrlEncurtado(linkRequest));
+    }
+
+    @GetMapping("/{id}")
+    public void redirecionaUrl(@PathVariable("id") String id, HttpServletResponse httpServlet) throws IOException {
+        Link link = linkService.procuraLink(id);
+
+        httpServlet.sendRedirect(link.getOriginalUrl());
     }
 }
