563f5c7d6bbfc37b981bb0afc1a1e70bc60a7b8b
Thullyoo <thullyofilho@gmail.com>
2024-07-14 20:12:01 -0300

    feat: criado metodo Post para criação de url encurtada

--- src/main/java/com/thullyoo/encurtaUrl/controllers/LinkController.java
+++ src/main/java/com/thullyoo/encurtaUrl/controllers/LinkController.java
@@ -1,8 +1,24 @@
 package com.thullyoo.encurtaUrl.controllers;
 
+import com.thullyoo.encurtaUrl.entities.LinkRequest;
+import com.thullyoo.encurtaUrl.entities.LinkResponse;
+import com.thullyoo.encurtaUrl.services.LinkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LinkController {
+
+    @Autowired
+    private LinkService linkService;
+
+    @PostMapping()
+    public ResponseEntity<LinkResponse> criarUrlEncurtada(@RequestBody LinkRequest linkRequest){
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.linkService.criarUrlEncurtado(linkRequest));
+    }
 }
