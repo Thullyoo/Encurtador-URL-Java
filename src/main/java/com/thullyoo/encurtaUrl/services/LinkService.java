762fefd35f254a6c6b0b841f16e766eebc2bbbd7
Thullyoo <thullyofilho@gmail.com>
2025-03-31 16:00:42 -0300

    feat: add function to generate qr code from shortened link

--- src/main/java/com/thullyoo/encurtaUrl/services/LinkService.java
+++ src/main/java/com/thullyoo/encurtaUrl/services/LinkService.java
@@ -1,5 +1,10 @@
 package com.thullyoo.encurtaUrl.services;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import com.thullyoo.encurtaUrl.entities.Link;
 import com.thullyoo.encurtaUrl.entities.LinkRequest;
 import com.thullyoo.encurtaUrl.entities.LinkResponse;
@@ -8,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.apache.commons.lang3.RandomStringUtils;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.OptionalInt;
@@ -34,9 +41,26 @@ public class LinkService {
         link.setHoraCriação(LocalDateTime.now());
         link.setExpiraEm(link.getHoraCriação().plusMinutes(5));
 
+        try {
+            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+            BitMatrix bitMatrix = qrCodeWriter.encode("http://localhost:8080/" + id, BarcodeFormat.QR_CODE, 500, 350);
+
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            MatrixToImageWriter.writeToStream(bitMatrix, "PNG", byteArrayOutputStream);
+
+            link.setQrCode(byteArrayOutputStream.toByteArray());
+
+
+        } catch (WriterException | IOException e) {
+
+            throw new RuntimeException(e.getMessage());
+
+        }
+
         linkRepository.save(link);
 
         return new LinkResponse("http://localhost:8080/" + id);
+
     }
 
 
@@ -51,4 +75,5 @@ public class LinkService {
             return link.get();
 
     }
+
 }
